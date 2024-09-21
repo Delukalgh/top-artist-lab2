2087e235ea1236a996fee090c3ae51db1940da6f
Deluka <deluka.lgh@gmail.com>
2024-09-20 17:10:29 -0700

    adding content for Lab 2 front end web development

---
+++ src/artists.js
@@ -0,0 +1,29 @@
+export const ARTISTS = [
+  {
+    name:"Post Malone",
+    image:"https://compote.slate.com/images/a6a3918e-5388-4d4b-971d-cd2916e60ab1.jpeg?crop=1560%2C1040%2Cx0%2Cy0",
+    country:"American Singer Songwriter",
+    years:"2015 - Present"
+  },
+  {
+    name:"Agust D",
+    image:"https://assets.teenvogue.com/photos/5ed0091e4e22a58f3f6de797/16:9/w_2560%2Cc_limit/agust%2520d%2520try%25203.jpg",
+    country:"Korean Rapper",
+    years:"2016 - Present"
+  },  {
+    name:"Keshi",
+    image:"https://billboardphilippines.com/wp-content/uploads/2024/07/keshi-ftr-img.jpg",
+    country:"American Singer Songwriter",
+    years:"2019 - Present"
+  },  {
+    name:"Yama",
+    image:"https://japannews.yomiuri.co.jp/wp-content/uploads/2024/03/11065598.jpg",
+    country:"Japanese Singer",
+    years:"2018 - Present"
+  },  {
+    name:"Vaundy",
+    image:"https://i0.wp.com/unis-son.com/wp-content/uploads/2024/01/Vaundy.jpeg?fit=1000%2C1000&ssl=1",
+    country:"Singer, Songwriter",
+    years:"2019 - Present"
+  },
+]
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="header">
+            <h1>My Favorite Artists on One Page!</h1>
+        </div>
+    )
+}
+
+export default Header;
